38872b4a385cca88087b24506a954280bd088514
adr <adr.crespo93@gmail.com>
2025-04-18 23:53:25 +0200

    Header y bloque eventos estáticos hechos

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -4,24 +4,139 @@
 
 <template>
     <!-- contenedor principal -->
-    <div class="background-container bg-white w-full h-screen flex flex-col">
+    <div class="background-container bg-white relative overflow-hidden w-full mx-auto min-h-screen flex flex-col">
 
-        <header class="w-full max-w-[1512px] flex justify-between items-center mx-auto px-4 py-2 bg-primary text-white">
-            <!-- logo -->
-            <div class="flex items-center">
-                <img src="@assets/logo.png" alt="Logo jardín del despertar" class="w-12 h-12 mr-2">
+        <header class="z-20">
+            <!-- barra superior -->
+            <div class="py-1 bg-yellow-600 w-full">
+                <div class="flex items-center justify-between max-w-[1512px] w-[90%] mx-auto px-4">
+                    <div><a href="#" class="text-white hover:text-gray-300">Entrar en contacto</a></div>
+                    <div><a href="#" class="text-white hover:text-gray-300">¡Hazte socio!</a></div>
+                </div>
+            </div>
+            <div class="w-full bg-primary h-[100px] flex items-center">
+                <div class="max-w-[1512px] w-[90%] flex justify-between items-center mx-auto px-4 py-2 text-white">
+                    <!-- logo -->
+                    <div class="flex items-center">
+                        <a href="#"><img src="@assets/logo.png" alt="Logo jardín del despertar"
+                                class="w-20 rounded-full h-full mr-2"></a>
+                    </div>
+
+                    <!-- navegador -->
+                    <nav class="flex items-center">
+                        <ul class="flex space-x-4">
+                            <li><a href="#" class="text-white hover:text-gray-300">Quienes somos</a></li>
+                            <li><a href="#" class="text-white hover:text-gray-300">Eventos</a></li>
+                            <li><a href="#" class="text-white hover:text-gray-300">Aulas</a></li>
+                            <li><a href="#" class="text-white hover:text-gray-300">Nuestra tienda</a></li>
+                            <li><a href="#" class="text-white hover:text-gray-300">Novedades</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+
+        <main class="z-1">
+            <!-- banner -->
+            <div class="w-full max-w-[1512px] mx-auto relative">
+                <!-- video -->
+                <div class="flex justify-center items-center">
+                    <video class="w-full max-w-[1512px] max-h-[700px] object-cover object-top" autoplay muted loop>
+                        <source src="@assets/home_banner.mp4" type="video/mp4" />
+                        Tu navegador no soporta la etiqueta de video.
+                    </video>
+                </div>
+
+                <!-- texto en el video -->
+                <div
+                    class="absolute top-40 right-10 hidden md:flex flex-col items-center text-center text-[#320000]  drop-shadow-[0_1px_1px_rgba(255,255,255,0.7)]">
+                    <h1 class="text-4xl lg:text-6xl font-bold">Jardín del Despertar</h1>
+                    <p class="text-3xl font-bold mt-2">Un lugar de paz</p>
+                </div>
+
+                <!-- texto fuera del video (mobile) -->
+                <div class="block md:hidden mt-6 w-[80%] text-left text-[#320000] mx-auto">
+                    <h1 class="text-4xl font-bold">Jardín del Despertar</h1>
+                    <p class="text-lg font-normal mt-4">Un lugar de paz</p>
+                </div>
             </div>
 
-            <!-- navegador -->
-            <nav class="flex items-center">
-                <ul class="flex space-x-4">
-                    <li><a href="#" class="nav-item">Inicio</a></li>
-                    <li><a href="#" class="nav-item">Nosotros</a></li>
-                    <li><a href="#" class="nav-item">Servicios</a></li>
-                    <li><a href="#" class="nav-item">Contacto</a></li>
-                </ul>
-            </nav>
-        </header>
+            <!-- eventos -->
+            <div class="w-[90%] max-w-[1512px] mx-auto">
+                <h2 class="text-4xl font-bold text-center text-[#320000] my-10">Eventos</h2>
+
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+
+                    <div class="bg-amber-50 shadow-lg rounded-4xl overflow-hidden pb-6">
+
+                        <img src="@assets/meditacion.jpeg" alt="Evento de meditación"
+                            class="w-full h-68 object-cover rounded-t-lg">
+
+                        <h3 class="text-2xl font-semibold text-[#320000] text-center mt-6">
+                            Meditación para principiantes
+                        </h3>
+
+                        <p class="text-[#320000] text-lg p-4">
+                            Descubre la calma interior y aprende a meditar desde cero
+                            en un espacio seguro y acogedor. No necesitas experiencia previa, solo ganas de conectar
+                            contigo mismo y encontrar un momento de paz.
+                        </p>
+
+                        <button class="flex justify-center items-center w-full">
+                            <a href="#"
+                                class="bg-[#320000] text-white font-semibold py-2 px-4 rounded-lg hover:bg-[#5c0f0f] transition duration-300">
+                                Más información
+                            </a>
+                        </button>
+                    </div>
+
+                    <div class="bg-amber-50 shadow-lg rounded-4xl overflow-hidden pb-6">
+                        <img src="@assets/meditacion.jpeg" alt="Evento de meditación"
+                            class="w-full h-68 object-cover rounded-t-lg">
+                        <h3 class="text-2xl font-semibold text-[#320000] text-center mt-6">Meditación para principiantes
+                        </h3>
+                        <p class="text-[#320000] text-lg p-4">
+                            Descubre la calma interior y aprende a meditar desde cero
+                            en un espacio seguro y acogedor. No necesitas experiencia previa, solo ganas de conectar
+                            contigo mismo y encontrar un momento de paz.
+                        </p>
+                        <button class="flex justify-center items-center w-full">
+                            <a href="#"
+                                class="bg-[#320000] text-white font-semibold py-2 px-4 rounded-lg hover:bg-[#5c0f0f] transition duration-300">
+                                Más información
+                            </a>
+                        </button>
+                    </div>
+
+                    <div class="bg-amber-50 shadow-lg rounded-4xl overflow-hidden pb-6">
+                        <img src="@assets/meditacion.jpeg" alt="Evento de meditación"
+                            class="w-full h-68 object-cover rounded-t-lg">
+                        <h3 class="text-2xl font-semibold text-[#320000] text-center mt-6">Meditación para principiantes
+                        </h3>
+                        <p class="text-[#320000] text-lg p-4">
+                            Descubre la calma interior y aprende a meditar desde cero
+                            en un espacio seguro y acogedor. No necesitas experiencia previa, solo ganas de conectar
+                            contigo mismo y encontrar un momento de paz.
+                        </p>
+                        <button class="flex justify-center items-center w-full">
+                            <a href="#"
+                                class="bg-[#320000] text-white font-semibold py-2 px-4 rounded-lg hover:bg-[#5c0f0f] transition duration-300">
+                                Más información
+                            </a>
+                        </button>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- noticias -->
+            <div class="w-[90%] max-w-[1512px] mx-auto">
+                <h2 class="text-4xl font-bold text-center text-[#320000] my-10">Últimas noticias</h2>
+
+
+            </div>
+        </main>
 
     </div>
 </template>
@@ -29,5 +144,20 @@
 <style scoped>
 .bg-primary {
     background: #320000 !important;
+}
+
+.background-container::before {
+    content: "";
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    height: 100%;
+    width: 100%;
+    max-width: 1512px;
+    background: url("@assets/bg4.png") no-repeat left bottom fixed;
+    background-size: 1200px;
+    opacity: 0.2;
+    pointer-events: none;
 }
 </style>
