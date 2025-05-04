b2db688a5595b5d4842356260ccd3368ead3deb3
adr <adr.crespo93@gmail.com>
2025-05-04 12:36:38 +0200

    feat: añadir componente HeaderBanner y sección de eventos en la página de Eventos

--- resources/js/components/HeaderBanner.vue
+++ resources/js/components/HeaderBanner.vue
@@ -4,20 +4,26 @@ defineProps({
     pageTitle: String
 })
 </script>
-
 <template>
-    <div class="w-full mx-auto min-h-[400px] bg-cover bg-center" :style="{ backgroundImage: `url(${imageUrl})` }">
-        <div class="hidden max-w-[1200px] min-h-[400px] mx-auto md:flex flex-col items-center justify-center px-4">
-            <!-- Page title -->
+    <div class="w-full mx-auto min-h-[400px] bg-cover bg-center relative"
+        :style="{ backgroundImage: `url(${imageUrl})` }">
+        <!-- degradado -->
+        <div class="absolute inset-0 bg-gradient-to-l from-primary-color to-primary-color/10]">
+        </div>
+
+        <div
+            class="relative hidden max-w-[1200px] min-h-[400px] mx-auto md:flex flex-col items-center justify-center px-4">
+            <!-- page title -->
             <div
                 class="flex justify-start items-center flex-wrap w-full max-w-[350px] lg:max-w-[500px] ml-auto mb-2 h-full">
-                <div class="text-xl font-bold text-tertiary-color">{{ pageTitle }}</div>
+                <div class="text-xl font-bold text-tertiary-color"
+                    style="text-shadow: 1px 1px 4px var(--secondary-color);">{{ pageTitle }}</div>
             </div>
-            <!-- Title -->
+            <!-- title -->
             <div class="flex justify-end items-center flex-wrap w-full max-w-[350px] lg:max-w-[500px] ml-auto h-full">
                 <h1 class="text-5xl/16 lg:text-7xl/20 font-bold text-white">Templo Jardín del Despertar</h1>
             </div>
-            <!-- Subtitle -->
+            <!-- subtitle -->
             <div class="flex justify-end items-center flex-wrap w-full max-w-[350px] lg:max-w-[500px] ml-auto h-full">
                 <h2 class="text-2xl font-bold text-secondary-color mt-4">
                     Un espacio para la meditación y el crecimiento personal
@@ -25,17 +31,15 @@ defineProps({
             </div>
         </div>
     </div>
+
     <!-- mobile -->
     <div class="md:hidden max-w-[1200px] min-h-[400px] mx-auto flex flex-col items-center justify-center px-4">
-        <!-- Page title -->
         <div class="flex justify-center items-center mb-2">
             <div class="text-2xl font-bold text-tertiary-color">{{ pageTitle }}</div>
         </div>
-        <!-- Title -->
         <div class="flex justify-center items-center flex-wrap w-full">
             <h1 class="text-6xl/18 font-bold text-primary-color text-center">Templo Jardín del Despertar</h1>
         </div>
-        <!-- Subtitle -->
         <div class="flex justify-center items-center flex-wrap w-full h-full">
             <h2 class="text-2xl font-bold text-secondary-color text-center mt-4">
                 Un espacio para la meditación y el crecimiento personal
--- resources/js/pages/Events.vue
+++ resources/js/pages/Events.vue
@@ -1,12 +1,64 @@
 <script setup lang="ts">
+import HeaderBanner from '@/components/HeaderBanner.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 
+import EventsImage from '@assets/eventos-banner.jpg';
 
+defineProps({
+    events: Array as () => {
+        id: number,
+        title: string,
+        date: string,
+        description: string,
+        image: string
+    }[]
+})
+
+const upcomingEvent = events[0]
+const otherEvents = events.slice(1)
 </script>
 
 <template>
     <AppLayout>
+        <HeaderBanner :imageUrl="EventsImage" pageTitle="Eventos" class="bg-primary-color/50" />
 
+        <section class="w-full px-4 max-w-[1200px] mx-auto">
+            <h2 class="text-4xl font-bold text-center text-primary-color my-20">Proximo Evento</h2>
+            <div class="max-w-6xl mx-auto py-12 px-4 grid md:grid-cols-2 gap-6 items-center">
+                <img :src="upcomingEvent.image" alt="Evento principal"
+                    class="w-full h-80 object-cover rounded-xl shadow-lg" />
+                <div>
+                    <p class="text-sm text-secondary-color mb-2">{{ new Date(upcomingEvent.date).toLocaleDateString() }}
+                    </p>
+                    <h2 class="text-3xl font-bold text-primary-color mb-4">{{ upcomingEvent.title }}</h2>
+                    <p class="text-gray-700 mb-4">{{ upcomingEvent.description }}</p>
+                    <a :href="`/eventos/${upcomingEvent.id}`"
+                        class="inline-block bg-primary-color text-white px-6 py-2 rounded shadow hover:bg-opacity-90 transition">
+                        Más información
+                    </a>
+                </div>
+            </div>
+        </section>
+
+        <!-- Otros eventos -->
+        <section class="w-full px-4 max-w-[1200px] mx-auto">
+            <h2 class="text-4xl font-bold text-center text-primary-color my-20">Otros Eventos</h2>
+            <div class="max-w-6xl mx-auto py-6 px-4 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                <div v-for="event in otherEvents" :key="event.id"
+                    class="bg-white shadow rounded-lg overflow-hidden hover:shadow-md transition">
+                    <img :src="event.image" alt="event" class="w-full h-40 object-cover" />
+                    <div class="p-4">
+                        <p class="text-xs text-secondary-color mb-1">{{ new Date(event.date).toLocaleDateString() }}</p>
+                        <h3 class="text-lg font-semibold text-primary-color mb-2">{{ event.title }}</h3>
+                        <p class="text-sm text-gray-600 line-clamp-2">{{ event.description }}</p>
+                        <a :href="`/eventos/${event.id}`"
+                            class="text-sm text-primary-color font-medium hover:underline mt-2 inline-block">
+                            Ver evento
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </AppLayout>
 </template>
 
