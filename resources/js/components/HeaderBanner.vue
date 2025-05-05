c6be3776388a64430047fb822976f00900711ffd
adr <adr.crespo93@gmail.com>
2025-05-06 00:19:37 +0200

    feat: ocultar degradado en dispositivos móviles en el componente HeaderBanner

--- resources/js/components/HeaderBanner.vue
+++ resources/js/components/HeaderBanner.vue
@@ -8,7 +8,7 @@ defineProps({
     <div class="w-full mx-auto min-h-[400px] bg-cover bg-center relative"
         :style="{ backgroundImage: `url(${imageUrl})` }">
         <!-- degradado -->
-        <div class="absolute inset-0 bg-gradient-to-l from-primary-color to-primary-color/10]">
+        <div class="hidden md:block absolute inset-0 bg-gradient-to-l from-primary-color to-primary-color/10]">
         </div>
 
         <div
