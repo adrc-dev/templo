2689db2793590df1a3fc707781cfad33aec8f66a
adr <adr.crespo93@gmail.com>
2025-05-26 09:16:33 +0200

    feat: agregar componente de paginación y actualizar VideoSection para su uso

--- resources/js/components/adr/Aulas/VideoSection.vue
+++ resources/js/components/adr/Aulas/VideoSection.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import VideoList from './VideoList.vue';
-import Button from '@/components/ui/button/Button.vue';
+import Pagination from '@/components/ui/pagination/Pagination.vue';
 
 defineProps<{
     title: string,
@@ -12,11 +12,7 @@ defineProps<{
     <h2 class="text-4xl font-bold text-center text-primary-color my-20">{{ title }}</h2>
 
     <VideoList v-if="videos && videos.data && videos.data.length" :videos="videos.data" />
-
     <p v-else class="text-tertiary-color italic">No hay videos por el momento.</p>
 
-    <div class="md:col-span-2 mt-6 flex justify-center gap-2 mb-12">
-        <Button v-if="videos.prev_page_url" @click="$inertia.visit(videos.prev_page_url)">Anterior</Button>
-        <Button v-if="videos.next_page_url" @click="$inertia.visit(videos.next_page_url)">Siguiente</Button>
-    </div>
+    <Pagination :pagination="videos" class="mb-12" />
 </template>
---
+++ resources/js/components/ui/pagination/Pagination.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+
+defineProps<{
+    pagination: {
+        prev_page_url: string | null;
+        next_page_url: string | null;
+    };
+}>();
+</script>
+
+<template>
+    <div class="mt-6 flex justify-center gap-2">
+        <Button v-if="pagination.prev_page_url" @click="$inertia.visit(pagination.prev_page_url)">
+            Anterior
+        </Button>
+
+        <Button v-if="pagination.next_page_url" @click="$inertia.visit(pagination.next_page_url)">
+            Siguiente
+        </Button>
+    </div>
+</template>
