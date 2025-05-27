fc47249311dbd24e8898916fa97bb8a4c2bd9526
adr <adr.crespo93@gmail.com>
2025-05-27 15:19:07 +0200

    fix: aumentar el efecto del fade

--- resources/js/components/adr/LegalModal.vue
+++ resources/js/components/adr/LegalModal.vue
@@ -28,7 +28,7 @@ const close = () => emit('close');
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
-    transition: opacity 0.2s ease;
+    transition: opacity 0.5s ease;
 }
 
 .fade-enter-from,
