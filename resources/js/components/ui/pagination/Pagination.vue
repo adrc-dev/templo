1939e09ff31680c9e9ce9a11a79a2881b9631e24
adr <adr.crespo93@gmail.com>
2025-05-27 14:43:50 +0200

    feat: mejorar funcionamiento de la paginación

--- resources/js/components/ui/pagination/Pagination.vue
+++ resources/js/components/ui/pagination/Pagination.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Button from '@/components/ui/button/Button.vue';
+import { router } from '@inertiajs/vue3';
 
 defineProps<{
     pagination: {
@@ -7,15 +8,19 @@ defineProps<{
         next_page_url: string | null;
     };
 }>();
+
+function goToPage(url: string) {
+    router.visit(url, { preserveScroll: true });
+}
 </script>
 
 <template>
     <div class="mt-6 flex justify-center gap-2">
-        <Button v-if="pagination.prev_page_url" @click="$inertia.visit(pagination.prev_page_url)">
+        <Button v-if="pagination.prev_page_url" @click="goToPage(pagination.prev_page_url)">
             Anterior
         </Button>
 
-        <Button v-if="pagination.next_page_url" @click="$inertia.visit(pagination.next_page_url)">
+        <Button v-if="pagination.next_page_url" @click="goToPage(pagination.next_page_url)">
             Siguiente
         </Button>
     </div>
